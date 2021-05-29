303198dd026a3278afb7fb91e95dfde8b1f89c07
Muhammad_Bilal_Rizwan <mb92733@gmail.com>
2021-05-30 00:21:45 +0500

    Resume Form UI

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Form from './Components/Form';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Form />
     </View>
   );
 }
@@ -17,5 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
